4da3c1af96424c73fa5257d7b0bc829dc7a820a4
Kamille55-star <kamillerodriguesdeoliveira@gmail.com>
2026-08-11 16:25:09 -0300

    Adicionando mais livros. Depois vou trocar imagens e descrições

--- dados.js
+++ dados.js
@@ -16,140 +16,140 @@ let dados = [
     },
    {
       imagem: "https://a-static.mlcdn.com.br/450pxx450px/kit-3-livros-as-cronicas-dos-kane-rick-riordan-intrinseca/studiochihiro/11219024825/f145497dc4b80fc60d1e2e4448c76417.jpeg",
-      titulo: "As crônicas dos Kane.", //não mudei foto
+      titulo: "As crônicas dos Kane.", //mudei foto
       descricao: "Dois irmãos separados após a morte da mãe passam o dia com o pai. Mas após o pai roubar uma pedra preciosa e morrer tentando invocar Hórus os irmãos descobrem que a mitologia egípcia e outras mitologias são reais. E que eles precisam salvar o mundo.",
       link: "https://www.amazon.com.br/Cronicas-Piramide-Vermelha-Serpente-Volumes/dp/8900007661/ref=sr_1_4?crid=32BZE5NBLUZQW&dib=eyJ2IjoiMSJ9.PHHeqARn5aj7hNkMGTRGlFUmeI2vaaI-FmAiKyIxsuSPCUsV9Fh0nxhUHu6GpNFewST_T-6XxmpWSGbbzI2Z39cqmu8h2M55z-HnQ3d_cFqjCNWLwTpjcgB_AcB9kAjhGEgsm2KU9CRM0gI7uxQ_pg0AtGF5BL1zjRESOOXKip79aXhYXDSdjknNAwb8QLNUYU_bzRMLSbEP4q3JA2Zl15ZB7XL5SdlAYvtZtjJZKCe6Q_qfU532YHEaZ66_nxo8z0FCCLZCMkkj2nR92lR22yHeMjQmm64C3cB2WtBfe5Y.lvdm9YS_6tOfuE3V5t6Q8G0MpY_mpbmJHfSdrEVPIJc&dib_tag=se&keywords=as+cronicas+dos+kane&qid=1786139418&sprefix=cronica+dos+%2Caps%2C269&sr=8-4&ufe=app_do%3Aamzn1.fos.fcd6d665-32ba-4479-9f21-b774e276a678",
       combinacao: "https://m.media-amazon.com/images/S/aplus-media-library-service-media/2e5c5f58-ec5b-4d3b-bb97-af5a860afd92.__CR0,0,970,600_PT0_SX970_V1___.jpg"
     },
     {
       imagem: "https://i.pinimg.com/736x/05/0b/04/050b046c58b99a7fe69e2481c4a94675.jpg",
-      titulo: "Um desejo para nós dois.", //não mudei foto
+      titulo: "Um desejo para nós dois.", //não mudei foto nem descrição
       descricao: "Ela é amante de música clássica. Ele é um DJ com sinestesia. Após ela se decepcionar no primeiro show dele eles pensaram que nunca mais se encontrariam. Mas o destino gosta de brincar.",
       link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Annabeth_Chase",
       combinacao: "https://www.projetou.com.br/posts/wp-content/uploads/sites/6/2021/06/partenon-2-1024x704.jpg"
     },
     {
       imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTup87UwlCcBfAMRLkH9Bggv3FExvCjAG_VFw&s",
-      titulo: "O tom mais gelado de azul", //não mudei foto
+      titulo: "O tom mais gelado de azul", //não mudei foto 
       descricao: "Thalia Grace é filha da mortal Beryl Grace e do deus Zeus. Aos 12 anos foi transformada em um pinheiro quando ficou para trás para salvar Annabeth e Luke. Possui medo de altura e atualmente é uma caçadora de Ártemis. Possui a habilidade de controlar a névoa e a eletricidade. Seu defeito fatal é a ambição.",
       link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Thalia_Grace",
       combinacao: "https://pbs.twimg.com/media/Ep2RC6dXUAUkx4f.jpg" //não mudei descrição
     },
     {
       imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT02DYv88xf3pVlQ4XDxSfP46pDa8K0GvHMWQ&s",
-      titulo: "Sob a aurora de sangue", //não mudei foto
+      titulo: "A Esperança", //não mudei foto nem descrição
       descricao: "Filha do deus da guerra Ares com uma mortal desconhecida. É líder do chalé de Ares e gosta bastante de brigar. Demorou bastante para virar amiga do Percy. Seu defeito fatal provavelmente é o orgulho. Interesse amoroso: Chris Rodríguez.",
       link: "https://aminoapps.com/c/mitologicpt/page/item/clarisse-la-rue/mq8j_8JF0Ie0d7bKQMzPpzrga144DraeQ1",
       combinacao: "https://pm1.aminoapps.com/6475/ed59a93084fd637a1cf7c8c42e891477884b657e_hq.jpg"
     },
     {
       imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQbocEOIRUb068YL1radhLjNQUAkW3kSuH6Q&s",
-      titulo: "Amor e gelato", //não mudei foto
+      titulo: "Amor e gelato", //não mudei foto nem descrição
       descricao: "Deus dos mares, terremotos, cavalos, lagos, rios, tempestades, secas, inundações. É pai de Percy Jackson e rival de Athena. Contraparte romano: Netuno.",
       link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Poseidon",
       combinacao: "https://3cc791f55f295f1d.cdn.gocache.net/versada/img_blog/i1_13_21_1114_041018.jpg"
     },
     {
       imagem: "https://cdn.rickriordan.com/wp-content/uploads/2017/04/11230243/athenalongerguard-299x416.jpg",
-      titulo: "Harry Potter e a pedra filosofal", //não mudei foto
+      titulo: "Harry Potter e a pedra filosofal", //não mudei foto nem descrição
       descricao: "Deusa da sabedoria e da estratégia em guerras, da civilização, matemática, estratégia, guerra defensiva, do artesanato, das artes, habilidades, da inteligência e do brilhantismo, seu símbolo é uma coruja. Teve uma desavença com Aracne e depois disso todos os seus filhos adquiriram medo de aranhas. Olhos cinzentos e calculistas. Não gosta do Percy. Contraparte romano: Minerva.",
       link: "https://riordan.fandom.com/wiki/Athena",
       combinacao: "https://avesornamentaisjej.cdn.magazord.com.br/img/2024/03/produto/462/79-coruja-suindara-2.png?ims=600x600"
     },
     {
       imagem: "https://i.pinimg.com/564x/d7/ae/72/d7ae724aeb0af8b702e4d75008539d84.jpg",
-      titulo: "Vermelho é a cor do caos", //não mudei foto
+      titulo: "Vermelho é a cor do caos", //não mudei foto nem descrição
       descricao: "Deus do vinho, do teatro, da loucura, das festas e da alegria. Foi condenado a ficar no acampamento meio-sangue e foi proibido de beber vinho após perseguir uma ninfa duas vezes. Contraparte romano: Baco.",
       link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Dion%C3%ADsio",
       combinacao: "https://m.media-amazon.com/images/I/51wYv9ua2qL._AC_UF894,1000_QL80_.jpg"
     },
     {
       imagem: "https://static.wikia.nocookie.net/8f4d3252-c964-4d1b-90b7-a880c744b6ef/scale-to-width/755",
-      titulo: "O príncipe cruel", //não mudei foto
+      titulo: "O príncipe cruel", //não mudei foto nem descrição
       descricao: "Deusa da lareira, do fogo, do calor, do lar, da família, da vida doméstica, das oferendas, do estado e da crença. A mais velha dos 12 Olimpianos, ela desistiu de seu lugar no Olimpo. A atual guardiã de Elpis, o espírito da esperança. Contraparte romana: Vesta.",
       link: "https://acampamentomeiosangue.fandom.com/pt/wiki/H%C3%A9stia",
       combinacao: "https://modobrincar.rihappy.com.br/wp-content/uploads/2022/03/familia-feliz-980x500.jpeg"
     },
     {
       imagem: "https://i.pinimg.com/originals/72/84/3d/72843d89d4b180b5de2016af10d75001.jpg",
-      titulo: "Grover", //não mudei foto
+      titulo: "O rei peverso", //não mudei foto nem descrição
       descricao: "Grover foi o sátiro designado para cuidar de Thalia Grace e de Percy Jackson, se tornando um grande amigo do filho de Poseidon. Foi o primeiro e único buscador a encontrar Pã e se tornou membro do Conselho dos Anciãos de Casco Fendido. Pode dar bençãos aos animais, ler emoções, sentir cheiros de mosntros, deuses, semideuses e outras criaturas poderosas, é um excelente escalador e possui medo do subterrâneo e de ciclopes, e por isso não se dava bem com Tyson no início.",
       link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Grover_Underwood",
       combinacao:"https://images.tcdn.com.br/img/img_prod/992371/flauta_doce_de_bambu_em_g_sol_capa_gratis_15_1_5f6dfdc6cb82741dd7805b139331cfc3.jpg"
     },
     {
       imagem: "https://i.pinimg.com/originals/b5/68/7d/b5687d94b70eab3a49d7664ea3cb6bd0.jpg",
-      titulo: "Tyson", //não mudei foto
+      titulo: "A rainha do nada", //não mudei foto nem descrição
       descricao: "Tyson é um ciclope, sendo filho de Poseidon e meio-irmão de Percy. Um dos poucos ciclopes bons, gentil, doce, infantil, chorão. Possui grande habilidades de contrução, só usa força bruta para salvar os amigos e é muito inteligente. É um dos poucos ciclopes realmente reconhecidos e reclamados por Poseidon.",
       link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Tyson",
       combinacao: "https://s2.glbimg.com/5wnmNnB4POV5hGlrrljdNEKz7JE=/620x453/e.glbimg.com/og/ed/f/original/2013/03/05/shutterstock_111014786.jpg"
     },
     {
       imagem: "https://pm1.aminoapps.com/6508/432d26b81164c627a14d8cb2ea47a326f4a2dd81_128.jpg",
-      titulo: "Sra. O' Leary/ Sra O' leary", //não mudei foto
+      titulo: "Estilhaça-me", //não mudei foto nem descrição
       descricao: "Único cão infernal amigável existente. Seu dono inicialmente era Quintus (Dédalo), mas depois da morte de Dédalo o seu dono virou Percy. Ela é filha de Cérbero e Nix.",
       link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Sra.O%27Leary",
       combinacao: "https://i.pinimg.com/550x/d0/11/1c/d0111c80b1b36913ce48d48416ba958e.jpg"
     },
     {
       imagem: "https://camp-chb.weebly.com/uploads/1/9/5/0/19500693/112650710.png",
-      titulo: "Cães infernais", //não mudei foto
+      titulo: "Liberta-me", //não mudei foto nem descrição
       descricao: "Eles são originários dos Campos de Punição. Esses cães, podem farejar qualquer tipo de meio-sangue e são servos de Hades. São filhos de Cérbero e Nix.",
       link: "https://acampamentomeio-sangue.fandom.com/pt-br/wiki/C%C3%A3o_Infernal",
       combinacao: "https://i.pinimg.com/736x/df/f2/3a/dff23acf9f0003d2210db452c0fe7a92.jpg"
     },
     {
       imagem: "https://i.pinimg.com/564x/c2/a2/4c/c2a24cdf1e3e1928dfeba217ae875675.jpg",
-      titulo: "Empousai (Empusa ou Empousa)", //não mudei foto
+      titulo: "Incendeia-me", //não mudei foto nem descrição
       descricao: "Filhas da deusa Hécate e do espírito Mormo, possuem cabelos flamejantes, uma perna de bode e outra perna de Bronze celestial. As Empusas são enviadas por Hécate para vigiar estradas e devorar viajantes. Criadas pela combinação de magia negra, animal, bronze e fantasma.",
       link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Empusa",
       combinacao: "#"
     },
     {
       imagem: "https://i.pinimg.com/736x/93/11/51/93115197640ea3960ab48447ce7f4324.jpg",
-      titulo: "Píton/Python/Piton", //não mudei foto
+      titulo: "Restaura-me", //não mudei foto nem descrição
       descricao: "Serpente monstruosa que foi morta por Apolo e foi revivida por Gaia para destruir o oráculo de Delfos. Nasceu quando Zeus inundou o mundo para punir os humanos. Quando Leto foi na Terra procurando um lugar para dar à luz depois que Hera a amaldiçoou, Python a expulsou.",
       link: "https://riordan.fandom.com/wiki/Python",
       combinacao: "#"
     },
     {
       imagem: "https://pm1.aminoapps.com/7184/0082ff2eb8218cbb11ae2d876cad9cc602f2b629r1-591-475v2_00.jpg",
-      titulo: "Mymerkos", //não mudei foto
+      titulo: "Desafie-me", //não mudei foto nem descrição
       descricao: "Formigas gigantes com carcaças extremamente resistentes e rubras. Suas pinças são usadas como armas, secretando veneno e uma gosma pegajosa. Habitam fortalezas ou subterrâneos. São atraídas por metais, principalmente ouro.",
       link: "https://aminoapps.com/c/percy-jackson-br/page/item/myrmekos/wkNm_vTpIJa6GLrQKm2jk2404dxpQl4qb",
       combinacao: "#"
     },
     {
       imagem: "https://i.pinimg.com/736x/58/64/53/586453a99ff8bcfad8d546eedae8d185.jpg",
-      titulo: "Jason Grace", //não mudei foto
+      titulo: "Imagina-me", //não mudei foto nem descrição
       descricao: "Filho de Júpiter (contraparte romano de Zeus) e Beryl Grace, é irmão mais novo de Thalia Grace. Pode controlar os ventos. Foi treinado por Lupa e virou pretor do acampamento romano.",
       link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Jason_Grace",
       combinacao: "https://i.quotev.com/b3zu7xekaaaa.jpg"
     },
     {
       imagem: "https://i.pinimg.com/564x/ca/ab/81/caab8106e423574c6add3d6719753b64.jpg",
-      titulo: "Frank Zhang", //não mudei foto
+      titulo: "Contos - Destrua-me, Fragmenta-me, Sombra-me, Decifra-me e Aceita-me", //não mudei foto nem descrição
       descricao: "Filho de Marte (contraparte romano de Ares) com a mortal Emily Zhang, possui alergia a lactose e por muito tempo teve sua vida ligada à um graveto. É um descendente de Periclimeno, um príncipe grego neto de Poseidon, sendo um semideus e um legado. abençoado com a habilidade de mudar de forma da sua família, além de suas habilidades como um filho de Marte. Primeiro semideus conhecido a não possuir TDAH e dislexia.",
       link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Frank_Zhang",
       combinacao: "https://pm1.aminoapps.com/7102/fbe6ec1402ec3f74484b4b18e4ce154e59bb9c54r1-750-522v2_uhq.jpg"
     },
     {
       imagem: "https://i.pinimg.com/474x/e2/99/29/e29929eb4257c79a1a829ca767088d89.jpg",
-      titulo: "Hazel Levesque", //não mudei foto
+      titulo: "O jardim das torres", //não mudei foto nem descrição
       descricao: "Filha de Plutão (contraparte romana de Hades) e Marie Levesque. É meia-irmã de Nico e Bianca Di Angelo. Pode controlar pedras preciosas e morreu em 1941, e iria para os campos Elísios, mas se sacrificou pela mãe, aceitando a punição dela em troca de deixá-la ilesa. Reviveu 70 anos depois, quando Nico Di Angelo a encontrou enquanto procurava Bianca Di Angelo.",
       link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Hazel_Levesque#Vida_Pregressa",
       combinacao: "https://img.freepik.com/fotos-premium/pedra-de-ouro-de-ouro-bruto-em-macro-fotografia-de-tesouro-escondido-de-mina-de-ouro_72932-5768.jpg"
     },
     {
       imagem: "https://i.pinimg.com/564x/d9/04/8b/d9048b12ce8847aac70937b452ca2d5e.jpg",
-      titulo: "Reyna Avila", //não mudei foto
+      titulo: "Jogos Vorazes", //não mudei foto nem descrição
       descricao: "Reyna Avila Ramírez-Arellano é uma semideusa filha de Belona e irmã de Hylla Ramírez-Arellano, a atual rainha das Amazonas. Reyna foi pretora do Acampamento Júpiter por quatro anos, até se aposentar de seu cargo e jurar lealdade a Ártemis, se juntando as caçadoras. É sempre acompanhada por dois cachorros autômatos feitos de ouro e prata, respectivamente Aurum e Argentium.",
       link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Reyna_Avila_Ram%C3%ADrez-Arellano",
       combinacao: "https://static.quizur.com/i/b/5938c58770f933.03056855pixiz-08-06-2017-05-33-11.jpg"
     },
     {
       imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQl_lkWCjgAcgy7ENu_eynjTXy-PSTdiC-TQQ&s",
-      titulo: "Sally Jackson", //não mudei foto
+      titulo: "Em chamas", //não mudei foto nem descrição
       descricao:"Mãe de Percy Jackson, é uma mulher gentil e uma excelente mãe. Inventou o lance da comida azul com o Percy. Após se 'separar' de Gabe conheceu Paul, com quem se casou e agora possui uma filha chamada Estelle." ,
       link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Sally_Jackson",
       combinacao: "https://pm1.aminoapps.com/7054/ba384b0eba6eff0cb254ab2edc10daebcdff5c37r1-1200-1800v2_hq.jpg"
