215c4b5d3e04683b6ba46185faf181e9bcbedd94
Kamille55-star <kamillerodriguesdeoliveira@gmail.com>
2026-08-23 12:21:10 -0300

    Adicionando autores

--- dados.js
+++ dados.js
@@ -2,6 +2,7 @@ let dados = [
     {
       imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSpu8ZNKeFS2N_k2KGlYIovM2_wAzsvvCg0A&s",
       titulo: "Percy Jackson e os Olimpianos",
+      autor: "Rick Riordan",
       descricao: "Escrito por Rick Riordan é uma série de 5 livros sobre um garoto que descobre que é um semideus e precisa salvar o mundo de Cronos",
       link: "https://www.amazon.com.br/Box-Percy-Jackson-olimpianos-edi%C3%A7%C3%A3o/dp/8551008889/ref=sr_1_1?crid=147FXC113YXCG&dib=eyJ2IjoiMSJ9.mwpWO7SEOQOYZmkcn864j1n-yiqm8FDSyZbRtrLny7wSpAlMd11XRYOnWG7gtcC9_qCvD9BDgLFumZjjmhzClj95Pr83JhAHtjj21FbR644IUkpv7-9RmdsnMC717hQr3hiLASrpCzK30EPE64506I9JMCn7KyHEA1P7890lowWFs2Yzn1G7lJyhSjT4zqLd3rK0B2K5tuMWhq7eaF14OymnOzNFQ_0wpyzYK3Ad4_-G7fO18cdNZoiXIZPY9evVCYolQ8Cv6UooC9lkqLIaMWvJfcSL3xnUmoZ-n8ndI2A.31ERHRBz_RXcQXEFuEPOClvvuTptzH1YDnYvE5jzEcM&dib_tag=se&keywords=percy+jackson+e+os+olimpianos&qid=1759443937&sprefix=percy+jack%2Caps%2C336&sr=8-1",
       combinacao: "https://i.pinimg.com/originals/5c/e0/97/5ce0974e23c06b7e1a6902c2c6e3cdfc.jpg",
@@ -10,6 +11,7 @@ let dados = [
     {
       imagem: "https://m.media-amazon.com/images/I/71pUZ9GBljL._UF1000,1000_QL80_.jpg",
       titulo:"Heróis do Olimpo",
+      autor: "Rick Riordan",
       descricao: "Heróis de Percy e os Olimpianos se juntam a novos heróis para salvar o mundo de Gaia",
       link: "https://a.co/d/iPnetIo", //mudei foto
       combinacao: "https://i.pinimg.com/originals/da/5b/7a/da5b7aec367c40be3d493aea7e9cf9aa.jpg",
@@ -17,6 +19,7 @@ let dados = [
    {
       imagem: "https://a-static.mlcdn.com.br/450pxx450px/kit-3-livros-as-cronicas-dos-kane-rick-riordan-intrinseca/studiochihiro/11219024825/f145497dc4b80fc60d1e2e4448c76417.jpeg",
       titulo: "As crônicas dos Kane.", //mudei foto
+      autor: "Rick Riordan",
       descricao: "Dois irmãos separados após a morte da mãe passam o dia com o pai. Mas após o pai roubar uma pedra preciosa e morrer tentando invocar Hórus os irmãos descobrem que a mitologia egípcia e outras mitologias são reais. E que eles precisam salvar o mundo.",
       link: "https://www.amazon.com.br/Cronicas-Piramide-Vermelha-Serpente-Volumes/dp/8900007661/ref=sr_1_4?crid=32BZE5NBLUZQW&dib=eyJ2IjoiMSJ9.PHHeqARn5aj7hNkMGTRGlFUmeI2vaaI-FmAiKyIxsuSPCUsV9Fh0nxhUHu6GpNFewST_T-6XxmpWSGbbzI2Z39cqmu8h2M55z-HnQ3d_cFqjCNWLwTpjcgB_AcB9kAjhGEgsm2KU9CRM0gI7uxQ_pg0AtGF5BL1zjRESOOXKip79aXhYXDSdjknNAwb8QLNUYU_bzRMLSbEP4q3JA2Zl15ZB7XL5SdlAYvtZtjJZKCe6Q_qfU532YHEaZ66_nxo8z0FCCLZCMkkj2nR92lR22yHeMjQmm64C3cB2WtBfe5Y.lvdm9YS_6tOfuE3V5t6Q8G0MpY_mpbmJHfSdrEVPIJc&dib_tag=se&keywords=as+cronicas+dos+kane&qid=1786139418&sprefix=cronica+dos+%2Caps%2C269&sr=8-4&ufe=app_do%3Aamzn1.fos.fcd6d665-32ba-4479-9f21-b774e276a678",
       combinacao: "https://m.media-amazon.com/images/S/aplus-media-library-service-media/2e5c5f58-ec5b-4d3b-bb97-af5a860afd92.__CR0,0,970,600_PT0_SX970_V1___.jpg"
@@ -24,6 +27,7 @@ let dados = [
     {
       imagem: "https://m.media-amazon.com/images/I/91wfhMZUa3L.jpg",
       titulo: "Um desejo para nós dois.", //mudei tudo certinho
+      autor: "Tillie Cole",
       descricao: "Ela é amante de música clássica. Ele é um DJ com sinestesia. Após ela se decepcionar no primeiro show dele eles pensaram que nunca mais se encontrariam. Mas o destino gosta de brincar.",
       link: "https://a.co/d/029rfR5s",
       combinacao: "https://m.media-amazon.com/images/I/91wfhMZUa3L.jpg"
@@ -31,6 +35,7 @@ let dados = [
     {
       imagem: "https://static.skeelo.com/remote/320/480/100/https://skoob.s3.amazonaws.com/livros/122450777/O_TOM_MAIS_GELADO_DE_AZUL_1714657030122450777SK-V11714657031B.jpg",
       titulo: "O tom mais gelado de azul", //mudei tudo certinho
+      autor: "Az. Florence",
       descricao: "Uma soez que foi adotada por Imáculas e vive como uma imácula. Vive perto da realeza, seu melhor amigo é o terceiro príncipe do reino. Mas tudo muda quando criaturas aparecem em um baile, matando sua mãe adotiva e despertando nela uma magia que ela não sabia que possuía. Em um reino que odeia magia. Agora ela precisa salvar o reino e descobrir quem ela realmente é.",
       link: "https://a.co/d/0a8xCkNC",
       combinacao: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIA_1PBdEfDMQ5yckVnxeHOJa7URs-hdOY2q77-oqpttshxHOo-IA54Mga&s=10" //não mudei descrição
@@ -38,6 +43,7 @@ let dados = [
     {
       imagem: "https://m.media-amazon.com/images/I/715ejknjS4L._UF1000,1000_QL80_.jpg",
       titulo: "A Esperança", //mudei tudo certinho
+      autor: "Suzanne Collins",
       descricao: "Peeta foi levado pela capital. E está diferente. Depois que o resgatam descobrem que a capital fez lavagem cerebral no Peeta. A revolução fica mais forte, mas Katniss percebe que talvez a líder da revolução se pareça mais com o Snow do que ela pensava. Em meio à guerra, Katniss é traída e perde pessoas importantes. Será que ela e Peeta vão conseguir sobreviver a guerra e a capital? E será que a saúde mental deles estará boa depois que tudo acabar? Descubra nesse último livro da trilogia Jogos Vorazes.",
       link: "https://a.co/d/09rTrNd6",
       combinacao: "https://m.media-amazon.com/images/S/pv-target-images/e0f41d9eb424509cfc055a786aad5b67c7dc5ca3f5288cfa932dbbadd52d2ed2._SX1080_FMjpg_.jpg"
@@ -45,6 +51,7 @@ let dados = [
     {
       imagem: "https://m.media-amazon.com/images/I/81EE1CAUoEL.jpg",
       titulo: "Amor e gelato", //mudei tudo certinho
+      autor: "Jenna Evans Welch",
       descricao:"Lina viaja para a Itália para cumprir o último desejo de sua mãe. Mas ao chegar lá ela descobre que sua mãe tinha um segredo e que ela precisa descobrir a verdade sobre sua mãe e sobre seu pai. E no meio disso tudo ela conhece Ren, um garoto misterioso que vai ajudá-la a descobrir a verdade. Mas ela também conhece Thomas, um garoto que, assim como Ren, vai fazê-la se apaixonar. E no meio disso tudo ela precisa descobrir quem ela realmente é e o que ela realmente quer. (PS: Não assistam ao filme da Netlix, é um horror, não tem nada a ver com o livro, é uma adaptação horrível e sem sentido. Leiam o livro, é maravilhoso, mil vezes melhor que o filme. O filme é tão ruim que, sempre que vão falar de adaptações ruins de livros na comunidade literária, amor e gelato é um dos primeiros a serem citados.)",
       link: "https://a.co/d/0eqh9mJ6",
       combinacao: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnvf8KZT6kAJhMYHawRfj8Dxvfy11AkgCl3ATq3oJfJ9tU8Ql279dGnxDy&s=10"
@@ -52,6 +59,7 @@ let dados = [
     {
       imagem: "https://m.media-amazon.com/images/I/81ibfYk4qmL.jpg",
       titulo: "Harry Potter e a pedra filosofal", //mudei tudo certinho
+      autor: "J.K. Rowling",
       descricao: "Harry Potter é um garoto órfão que vive com seus tios e seu primo, que o maltratam. No seu aniversário de 11 anos ele descobre que é um bruxo e que foi aceito na escola de magia e bruxaria de Hogwarts. Lá ele conhece novos amigos, aprende magia e descobre que é famoso no mundo bruxo por ter sobrevivido a um ataque do bruxo das trevas mais temido de todos os tempos, Lord Voldemort. Todos pensavam que Voldemort havia morrido, mas ele sobreviveu e está tentando voltar ao poder. Harry precisa enfrentar Voldemort e seus seguidores, os Comensais da Morte, para proteger seus amigos e o mundo bruxo.Todos pensavam que Voldemort estava morto, mas talvez ele não esteja tão morto assim. Harry conseguirá enfrentá-lo e proteger seus amigos e o mundo bruxo? Descubra nesse primeiro livro da série Harry Potter.",
       link: "https://a.co/d/06iK5cyH",
       combinacao: "https://m.media-amazon.com/images/I/81uRUnI9Y3L._AC_UF1000,1000_QL80_.jpg"
@@ -59,6 +67,7 @@ let dados = [
     {
       imagem: "https://i.pinimg.com/564x/d7/ae/72/d7ae724aeb0af8b702e4d75008539d84.jpg",
       titulo: "Vermelho é a cor do caos", //não mudei foto nem descrição
+      autor: "A.Z Florence",
       descricao: "Deus do vinho, do teatro, da loucura, das festas e da alegria. Foi condenado a ficar no acampamento meio-sangue e foi proibido de beber vinho após perseguir uma ninfa duas vezes. Contraparte romano: Baco.",
       link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Dion%C3%ADsio",
       combinacao: "https://m.media-amazon.com/images/I/51wYv9ua2qL._AC_UF894,1000_QL80_.jpg"
@@ -66,6 +75,7 @@ let dados = [
     {
       imagem: "https://static.wikia.nocookie.net/8f4d3252-c964-4d1b-90b7-a880c744b6ef/scale-to-width/755",
       titulo: "O príncipe cruel", //não mudei foto nem descrição
+      autor: "Holly Black",
       descricao: "Deusa da lareira, do fogo, do calor, do lar, da família, da vida doméstica, das oferendas, do estado e da crença. A mais velha dos 12 Olimpianos, ela desistiu de seu lugar no Olimpo. A atual guardiã de Elpis, o espírito da esperança. Contraparte romana: Vesta.",
       link: "https://acampamentomeiosangue.fandom.com/pt/wiki/H%C3%A9stia",
       combinacao: "https://modobrincar.rihappy.com.br/wp-content/uploads/2022/03/familia-feliz-980x500.jpeg"
@@ -73,6 +83,7 @@ let dados = [
     {
       imagem: "https://i.pinimg.com/originals/72/84/3d/72843d89d4b180b5de2016af10d75001.jpg",
       titulo: "O rei peverso", //não mudei foto nem descrição
+      autor: "Holly Black",
       descricao: "Grover foi o sátiro designado para cuidar de Thalia Grace e de Percy Jackson, se tornando um grande amigo do filho de Poseidon. Foi o primeiro e único buscador a encontrar Pã e se tornou membro do Conselho dos Anciãos de Casco Fendido. Pode dar bençãos aos animais, ler emoções, sentir cheiros de mosntros, deuses, semideuses e outras criaturas poderosas, é um excelente escalador e possui medo do subterrâneo e de ciclopes, e por isso não se dava bem com Tyson no início.",
       link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Grover_Underwood",
       combinacao:"https://images.tcdn.com.br/img/img_prod/992371/flauta_doce_de_bambu_em_g_sol_capa_gratis_15_1_5f6dfdc6cb82741dd7805b139331cfc3.jpg"
@@ -80,6 +91,7 @@ let dados = [
     {
       imagem: "https://i.pinimg.com/originals/b5/68/7d/b5687d94b70eab3a49d7664ea3cb6bd0.jpg",
       titulo: "A rainha do nada", //não mudei foto nem descrição
+      autor: "Holly Black",
       descricao: "Tyson é um ciclope, sendo filho de Poseidon e meio-irmão de Percy. Um dos poucos ciclopes bons, gentil, doce, infantil, chorão. Possui grande habilidades de contrução, só usa força bruta para salvar os amigos e é muito inteligente. É um dos poucos ciclopes realmente reconhecidos e reclamados por Poseidon.",
       link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Tyson",
       combinacao: "https://s2.glbimg.com/5wnmNnB4POV5hGlrrljdNEKz7JE=/620x453/e.glbimg.com/og/ed/f/original/2013/03/05/shutterstock_111014786.jpg"
@@ -87,6 +99,7 @@ let dados = [
     {
       imagem: "https://pm1.aminoapps.com/6508/432d26b81164c627a14d8cb2ea47a326f4a2dd81_128.jpg",
       titulo: "Estilhaça-me", //não mudei foto nem descrição
+      autor: "Tahereh Mafi",
       descricao: "Único cão infernal amigável existente. Seu dono inicialmente era Quintus (Dédalo), mas depois da morte de Dédalo o seu dono virou Percy. Ela é filha de Cérbero e Nix.",
       link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Sra.O%27Leary",
       combinacao: "https://i.pinimg.com/550x/d0/11/1c/d0111c80b1b36913ce48d48416ba958e.jpg"
@@ -94,6 +107,7 @@ let dados = [
     {
       imagem: "https://camp-chb.weebly.com/uploads/1/9/5/0/19500693/112650710.png",
       titulo: "Liberta-me", //não mudei foto nem descrição
+      autor: "Tahereh Mafi",
       descricao: "Eles são originários dos Campos de Punição. Esses cães, podem farejar qualquer tipo de meio-sangue e são servos de Hades. São filhos de Cérbero e Nix.",
       link: "https://acampamentomeio-sangue.fandom.com/pt-br/wiki/C%C3%A3o_Infernal",
       combinacao: "https://i.pinimg.com/736x/df/f2/3a/dff23acf9f0003d2210db452c0fe7a92.jpg"
@@ -101,6 +115,7 @@ let dados = [
     {
       imagem: "https://i.pinimg.com/564x/c2/a2/4c/c2a24cdf1e3e1928dfeba217ae875675.jpg",
       titulo: "Incendeia-me", //não mudei foto nem descrição
+      autor: "Tahereh Mafi",
       descricao: "Filhas da deusa Hécate e do espírito Mormo, possuem cabelos flamejantes, uma perna de bode e outra perna de Bronze celestial. As Empusas são enviadas por Hécate para vigiar estradas e devorar viajantes. Criadas pela combinação de magia negra, animal, bronze e fantasma.",
       link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Empusa",
       combinacao: "#"
@@ -108,6 +123,7 @@ let dados = [
     {
       imagem: "https://i.pinimg.com/736x/93/11/51/93115197640ea3960ab48447ce7f4324.jpg",
       titulo: "Restaura-me", //não mudei foto nem descrição
+      autor: "Tahereh Mafi",
       descricao: "Serpente monstruosa que foi morta por Apolo e foi revivida por Gaia para destruir o oráculo de Delfos. Nasceu quando Zeus inundou o mundo para punir os humanos. Quando Leto foi na Terra procurando um lugar para dar à luz depois que Hera a amaldiçoou, Python a expulsou.",
       link: "https://riordan.fandom.com/wiki/Python",
       combinacao: "#"
@@ -115,6 +131,7 @@ let dados = [
     {
       imagem: "https://pm1.aminoapps.com/7184/0082ff2eb8218cbb11ae2d876cad9cc602f2b629r1-591-475v2_00.jpg",
       titulo: "Desafie-me", //não mudei foto nem descrição
+      autor: "Tahereh Mafi",
       descricao: "Formigas gigantes com carcaças extremamente resistentes e rubras. Suas pinças são usadas como armas, secretando veneno e uma gosma pegajosa. Habitam fortalezas ou subterrâneos. São atraídas por metais, principalmente ouro.",
       link: "https://aminoapps.com/c/percy-jackson-br/page/item/myrmekos/wkNm_vTpIJa6GLrQKm2jk2404dxpQl4qb",
       combinacao: "#"
@@ -122,6 +139,7 @@ let dados = [
     {
       imagem: "https://i.pinimg.com/736x/58/64/53/586453a99ff8bcfad8d546eedae8d185.jpg",
       titulo: "Imagina-me", //não mudei foto nem descrição
+      autor: "Tahereh Mafi",
       descricao: "Filho de Júpiter (contraparte romano de Zeus) e Beryl Grace, é irmão mais novo de Thalia Grace. Pode controlar os ventos. Foi treinado por Lupa e virou pretor do acampamento romano.",
       link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Jason_Grace",
       combinacao: "https://i.quotev.com/b3zu7xekaaaa.jpg"
@@ -129,6 +147,7 @@ let dados = [
     {
       imagem: "https://i.pinimg.com/564x/ca/ab/81/caab8106e423574c6add3d6719753b64.jpg",
       titulo: "Contos - Destrua-me, Fragmenta-me, Sombra-me, Decifra-me e Aceita-me", //não mudei foto nem descrição
+      autor: "Tahereh Mafi",
       descricao: "Filho de Marte (contraparte romano de Ares) com a mortal Emily Zhang, possui alergia a lactose e por muito tempo teve sua vida ligada à um graveto. É um descendente de Periclimeno, um príncipe grego neto de Poseidon, sendo um semideus e um legado. abençoado com a habilidade de mudar de forma da sua família, além de suas habilidades como um filho de Marte. Primeiro semideus conhecido a não possuir TDAH e dislexia.",
       link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Frank_Zhang",
       combinacao: "https://pm1.aminoapps.com/7102/fbe6ec1402ec3f74484b4b18e4ce154e59bb9c54r1-750-522v2_uhq.jpg"
@@ -143,6 +162,7 @@ let dados = [
     {
       imagem: "https://i.pinimg.com/564x/d9/04/8b/d9048b12ce8847aac70937b452ca2d5e.jpg",
       titulo: "Jogos Vorazes", //não mudei foto nem descrição
+      autor: "Suzanne Collins",
       descricao: "Reyna Avila Ramírez-Arellano é uma semideusa filha de Belona e irmã de Hylla Ramírez-Arellano, a atual rainha das Amazonas. Reyna foi pretora do Acampamento Júpiter por quatro anos, até se aposentar de seu cargo e jurar lealdade a Ártemis, se juntando as caçadoras. É sempre acompanhada por dois cachorros autômatos feitos de ouro e prata, respectivamente Aurum e Argentium.",
       link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Reyna_Avila_Ram%C3%ADrez-Arellano",
       combinacao: "https://static.quizur.com/i/b/5938c58770f933.03056855pixiz-08-06-2017-05-33-11.jpg"
@@ -150,6 +170,7 @@ let dados = [
     {
       imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQl_lkWCjgAcgy7ENu_eynjTXy-PSTdiC-TQQ&s",
       titulo: "Em chamas", //não mudei foto nem descrição
+      autor: "Suzanne Collins",
       descricao:"Mãe de Percy Jackson, é uma mulher gentil e uma excelente mãe. Inventou o lance da comida azul com o Percy. Após se 'separar' de Gabe conheceu Paul, com quem se casou e agora possui uma filha chamada Estelle." ,
       link: "https://acampamentomeiosangue.fandom.com/pt/wiki/Sally_Jackson",
       combinacao: "https://pm1.aminoapps.com/7054/ba384b0eba6eff0cb254ab2edc10daebcdff5c37r1-1200-1800v2_hq.jpg"
